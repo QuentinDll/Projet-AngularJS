4fbdab77eca85163d33c367222bd05b4bc496e26
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-18 08:30:11 +0200

    rattrapage panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,47 @@
 var app = angular.module('myApp', []);
-<<<<<<< HEAD
-app.controller('cardCtrl', function($scope, $http, $rootScope) {
-=======
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $rootScope) {
   $http.get("assets/js/phone.json").then(function (response)
   {$scope.phones = response.data});
-$scope.changeMarque = function(marque) {
+  $scope.changeMarque = function(marque) {
     $scope.filterChange = marque;
   };
   $scope.changeOrder = function(order) {
-        $scope.orderby = order;
-    };
+    $scope.orderby = order;
+  };
   });
-
-app.controller('cardCtrl', function($scope, $http) {
->>>>>>> 5322f00a8f39400ff368d2f93ed9a3a3af3ce5d7
-  $http.get('assets/js/phone.json').then(function (response)
-  {$scope.myTable = response.data});
-  $rootScope.storeItems = function (table) {
-    $rootScope.marque.push(table.marque);
-    $rootScope.modele.push(table.modele);
-    $rootScope.prix.push(table.prix);
-    $rootScope.image.push(table.image);
-  };
-  //récupération des données du tableau phone
-});
-app.run(['$rootScope', function($rootScope) {
-  $rootScope.marque = [];
-  $rootScope.modele = [];
-  $rootScope.prix = [];
-  $rootScope.image = [];
-  $rootScope.description = [];
-}]);
+  app.controller('cardCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope,$http) {
+    $http.get('assets/js/phone.json').then(function (response) {
+      $scope.myTable = response.data
+    });
+    $rootScope.storeItems = function (phone) {
+      $rootScope.marque.push(phone.marque);
+      $rootScope.modele.push(phone.modele);
+      $rootScope.prix.push(phone.prix);
+      $rootScope.image.push(phone.image);
+      $rootScope.description.push(phone.description);
+    };
+  }]);
+  app.run(['$rootScope', function($rootScope) {
+    $rootScope.marque = [];
+    $rootScope.modele = [];
+    $rootScope.prix = [];
+    $rootScope.image = [];
+    $rootScope.description = [];
+    $rootScope.qte = [];
+  }]);
 //fonction envoyant les données dans le panier (cart)
-app.controller('storeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 
+app.controller('storeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 }]);
-/*app.controller('storeCtrl', ['$http', function($http){
-  var phone = $http.get('assets/js/phone.json');
-  this.product = phone;
-  this.totalItems = 0;
-  this.storeItems = function() {
-    this.totalItems = this.totalItems + 1;
-  };
-}]);*/
+
+
+/*
+app.Controller ('storeController' ['$http', function($http){
+var phone = $http.get('assets/js/phone.json');
+this.product = phone;
+this.totalItems = 0;
+this.addItems = function() {
+this.totalItems = this.totalItems + 1;
+};
+}]);
+*/
